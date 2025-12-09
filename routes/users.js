d558c0706ad62332c54282c9e06ff581ba38ecf9
JanetK-143 <janetkoublanou@gmail.com>
2025-12-09 18:34:28 -0500

    Progress on user routes

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,19 @@
 import { Router } from "express";
 const router = Router();
-
+import * as userData from "../data/users.js";
+import { requireLogin, redirectIfLoggedIn } from "../middleware.js";
 // Placeholder routes — you can replace later
 
 router.get("/", (req, res) => {
   res.send("Users route placeholder");
 });
 
-router.get("/login", (req,res) => {
-  res.render('users/login');
-})
+router.get("/login", (req, res) => {
+  res.render("users/login");
+});
 
-router.get('/register', (req,res) => {
-  res.render('users/register');
+router.get("/register", (req, res) => {
+  res.render("users/register");
 });
 
 export default router;
