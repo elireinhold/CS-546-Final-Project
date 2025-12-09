1ea5306d305abc338ec7c2a9258111fc5d1472ce
Ruuuuuuuby <jru6055@gmail.com>
2025-12-09 02:45:38 -0500

    updated app.js and readme

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 import express from "express";
-import handlebars from "express-handlebars";
+import exphbs from "express-handlebars";
 import session from "express-session";
 import cookieParser from "cookie-parser";
 import configRoutes from "./routes/index.js";
 import { settings } from "./config/settings.js";
+import configMiddleware from './middleware.js';
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Session
 app.use(
   session({
     name: settings.session.cookieName,
@@ -22,41 +21,40 @@ app.use(
   })
 );
 
-// Handlebars setup (+ contains helper)
-const hbs = handlebars.create({
-  defaultLayout: "main",
-  helpers: {
-    ifEquals(a, b, options) {
-      return a === b ? options.fn(this) : options.inverse(this);
-    },
-    contains(arr, value) {
-      if (!arr) return false;
-      if (!Array.isArray(arr)) return arr === value;
-      return arr.includes(value);
-    },
-    toString(value) {
-      if (!value) return "";
-      return value.toString();
-    },
-    eq(a, b) {
-      return String(a) === String(b);
-    },
-    json(data) {
-      return JSON.stringify(data);
+app.engine(
+  "handlebars",
+  exphbs.engine({
+    defaultLayout: "main",
+    helpers: {
+      ifEquals(a, b, options) {
+        return a === b ? options.fn(this) : options.inverse(this);
+      },
+      contains(arr, value) {
+        if (!arr) return false;
+        if (!Array.isArray(arr)) return arr === value;
+        return arr.includes(value);
+      },
+      toString(value) {
+        return value ? value.toString() : "";
+      },
+      eq(a, b) {
+        return String(a) === String(b);
+      },
+      json(data) {
+        return JSON.stringify(data);
+      }
     }
-  }
-});
+  })
+);
 
-app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-// Static folder
 app.use("/public", express.static("public"));
 
-// Load routes
+configMiddleware(app);
+
 configRoutes(app);
 
-// Server
 app.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
 });
