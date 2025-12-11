fab2499b20ec8d4bf695da58c435918a47920fb1
Eli Reinhold <elireinhold1021@gmail.com>
2025-12-11 14:06:15 -0500

    Sped up load times for maps

--- data/map.js
+++ data/map.js
@@ -3,25 +3,60 @@ import { getEventById } from "./events.js";
 import { getSavedEvents } from "./users.js"
 import helpers from "../helpers/eventHelpers.js";
 
+// For geocodeLocation to speed up times
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-// Geocodes location and returns cooridinates
-export async function geocodeLocation(eventLocation) {
-  if(!eventLocation || typeof eventLocation !== "string"){
-    throw "Error: Invalid eventLocation"
+export const geocodeCache = new Map();
+
+// Geocodes location and returns coordinates
+export async function geocodeLocation(eventLocation, throttleMs = 0) {
+  if (!eventLocation || typeof eventLocation !== "string") {
+    throw "Error: Invalid eventLocation";
   }
-  const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(eventLocation)}`;
+
+  // Return cached result (speeds up loading times)
+  if (geocodeCache.has(eventLocation)) {
+    return geocodeCache.get(eventLocation);
+  }
+
+  // Limit the search location to just NYC (speeds up loading times)
+  const nycBounds = [-74.25909, 40.477399, -73.70018, 40.917577];
+
+  const url = `https://nominatim.openstreetmap.org/search?` +
+    new URLSearchParams({
+      q: eventLocation,
+      format: "json",
+      limit: "1",
+      addressdetails: "0",
+      bounded: "1",
+      viewbox: nycBounds.join(","),
+    });
+
   const res = await fetch(url, {
     headers: {
       "User-Agent": "NYSeeNowApp/1.0 (ereinhol@stevens.edu)"
     }
-  });  const data = await res.json();
-    if (!data[0]) return null;
-    
-    return {
-      lat: parseFloat(data[0].lat),
-      lon: parseFloat(data[0].lon)
-    };
-  }
+  });
+
+  const data = await res.json();
+  if (!data[0]) return null;
+
+  const coords = {
+    lat: parseFloat(data[0].lat),
+    lon: parseFloat(data[0].lon)
+  };
+
+  // Save location in cache
+  geocodeCache.set(eventLocation, coords);
+
+  // Throttle if needed
+  if (throttleMs > 0) await delay(throttleMs);
+
+  return coords;
+}
+
 
 // Uses geocoding to get all events with coordinates that user saved
 export async function getAllSavedEventsWithCoordinates(userId) {
