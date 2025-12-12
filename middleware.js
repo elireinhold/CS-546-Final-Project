913b0af42b68aa97c1740e3e948593e01362c4e8
JanetK-143 <janetkoublanou@gmail.com>
2025-12-12 10:46:47 -0500

    require login redirects to login page.

--- middleware.js
+++ middleware.js
@@ -12,10 +12,10 @@ export function redirectIfLoggedIn(req, res, next) {
 
 export function requireLogin(req, res, next) {
   if (!req.session.user) {
-    return res.status(403).json({ error: "You must be logged in to do this." });
+    return res.redirect("/users/login");
   }
   next();
-} 
+}
 
 export function requestLogger(req, res, next) {
   const time = new Date().toLocaleTimeString();
